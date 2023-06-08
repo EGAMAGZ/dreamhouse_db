1bcbb763ac1e11b3961c4664e72c4a021f1bff33
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-06-08 05:38:20 -0600

    Terminado modulo propietarios * Terminado modulo de propietarios * FOrmateado todos los SQL

---
+++ DR-HA-Propietarios/propietarios.sql
@@ -0,0 +1,81 @@
+-- DR-HA-CU5: GESTION DE ALQUILERES
+-- DR-HA-CU5.1: REGISTRO DE ALQUILERES
+INSERT INTO
+    Propiedades(
+        txt_ubi_propd,
+        num_no_hab_propd,
+        txt_tam_propd,
+        num_prec_propd,
+        txt_comp_propd,
+        fk_id_prop
+    )
+VALUES
+    (
+        'Avenida Jordania,Nueva México,19344',
+        5,
+        '30m2',
+        83217665,
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        5
+    );
+
+-- DR-HA-CU5.2: EDICION DE ALQUILERES
+UPDATE
+    Propiedades
+SET
+    num_no_hab_propd = 3,
+    txt_tam_propd = '24m2',
+    bool_act_propd = 1
+WHERE
+    pk_id_propd = 8;
+
+-- DR-HA-CU5.3: CONFIRMACION DE CITA DE ALQUILER
+UPDATE
+    Propiedades
+SET
+    bool_act_propd = 1
+WHERE
+    pk_id_propd = 8;
+
+INSERT INTO
+    Citas(
+        txt_fecha_cit,
+        txt_hora_cit,
+        fk_id_propd,
+        fk_id_cli
+    )
+VALUES
+    ('27/6/2023', '13:00', 8, 4);
+
+UPDATE
+    Citas
+SET
+    bool_confir_cit = 1
+WHERE
+    pk_id_cit = 4;
+
+-- DR-HA-CU5.4: GENERAR CONTRATO DE ALQUILER
+INSERT INTO
+    Contratos(
+        fk_id_cli,
+        fk_id_propd,
+        txt_cond_al_cont,
+        fk_id_sup,
+        txt_ini_cont,
+        txt_fin_cont,
+        txt_cond_com_cont,
+        num_pag_cont,
+        bool_val_cont
+    )
+VALUES
+    (
+        4,
+        8,
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        1,
+        '30/10/2022',
+        '06/06/2023',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        9481,
+        1
+    );
